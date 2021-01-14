378ecf45329009047bbb011982141bf1eb574da2
fuzzy69 <zeewolf@neobee.net>
2021-01-14 19:13:52 +0100

    Moved network manager logic to a separate class

---
+++ src/application/httpclient.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <QObject>
+
+class QNetworkAccessManager;
+class QNetworkReply;
+class QTimer;
+
+class HttpClient : public QObject
+{
+    Q_OBJECT
+    
+public:
+    explicit HttpClient(QObject *parent = nullptr);
+//     ~HttpClient();
+
+    QNetworkReply* head(const QUrl& url);
+    QNetworkReply* head(const QString& url);
+    QString userAgent() const;
+    void setUserAgent(const QString &userAgent);
+    int timeout() const;
+    void setTimeout(int seconds);
+
+signals:
+    void replyTimeout();
+    void replyFinished(int statusCode, const QString &statusText, const QString &text);
+
+protected slots:
+
+
+protected:
+    void onReplyTimeout();
+    void onReplyFinished(QNetworkReply *reply);
+
+private:
+    QNetworkAccessManager *m_networkManager;
+    QTimer *m_replyTimeoutTimer;
+    QNetworkReply *m_reply;
+    int m_replyTimeout;
+    QByteArray m_userAgent;
+};
