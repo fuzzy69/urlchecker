f4b01749712c38f0400e8943f22348b1116b4961
fuzzy69 <zeewolf@neobee.net>
2021-04-27 15:37:30 +0200

    Added several string/text processing functions

---
+++ src/application/utils/text.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+std::vector<std::string> split(const std::string& text, const std::string& delimiter);
+
+std::vector<std::string> split_lines(const std::string& text);
+
+std::string left_strip(const std::string& text);
+
+std::string right_strip(const std::string& text);
+
+std::string strip(const std::string& text);
+
+bool starts_with(const std::string& text, const std::string& prefix);
