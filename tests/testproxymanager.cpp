5f103a3591291f172b8c2b12bb47451127007555
fuzzy69 <zeewolf@neobee.net>
2021-04-27 15:42:28 +0200

    Updated classes which are singleton now

--- tests/testproxymanager.cpp
+++ tests/testproxymanager.cpp
@@ -9,12 +9,11 @@
 
 TEST_CASE( "Test ProxyManager class", "[proxymanager]" )
 {
-    ProxyManager proxyManager;
-    proxyManager.add_proxy(HttpProxy("127.0.0.1", 80));
-    proxyManager.add_proxy(HttpProxy("127.0.0.2", 80));
-    proxyManager.add_proxy(HttpProxy("127.0.0.3", 80));
-    auto proxy = proxyManager.get_proxy();
+    ProxyManager::instance().add_proxy(HttpProxy("127.0.0.1", 80));
+    ProxyManager::instance().add_proxy(HttpProxy("127.0.0.2", 80));
+    ProxyManager::instance().add_proxy(HttpProxy("127.0.0.3", 80));
+    auto proxy = ProxyManager::instance().get_proxy();
     REQUIRE(proxy.port() == 80);
     REQUIRE(proxy.ip() != "127.0.0.4");
-    REQUIRE(proxyManager.count() == 3);
+    REQUIRE(ProxyManager::instance().count() == 3);
 }
--- tests/testuseragentsmanager.cpp
+++ tests/testuseragentsmanager.cpp
@@ -9,12 +9,11 @@
 
 TEST_CASE( "Test UserAgentsManager class", "[useragentsmanager]" )
 {
-    UserAgentsManager<std::string> userAgentsManager;
-    userAgentsManager.add_user_agent("Firefox");
-    REQUIRE(userAgentsManager.get_random() == "Firefox");
-    REQUIRE(userAgentsManager.count() == 1);
-    UserAgentsManager<QString> userAgentsManager2;
-    userAgentsManager2.add_user_agent(QStringLiteral("Opera"));
-    REQUIRE(userAgentsManager2.get_random() == QString("Opera"));
-    REQUIRE(userAgentsManager2.count() == 1);
+    UserAgentsManager<std::string>::instance().add_user_agent("Firefox");
+    REQUIRE(UserAgentsManager<std::string>::instance().get_random() == "Firefox");
+    REQUIRE(UserAgentsManager<std::string>::instance().count() == 1);
+
+    UserAgentsManager<QString>::instance().add_user_agent("Opera");
+    REQUIRE(UserAgentsManager<QString>::instance().get_random() == "Opera");
+    REQUIRE(UserAgentsManager<QString>::instance().count() == 1);
 }
