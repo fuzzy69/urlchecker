ed91bba172641ef16c6f83e41eff2648f10e5b56
fuzzy69 <zeewolf@neobee.net>
2021-04-27 15:38:26 +0200

    Added several URL helper functions

--- src/application/utils/misc.h
+++ src/application/utils/misc.h
@@ -1,10 +1,18 @@
 #pragma once
 
+#include <string>
+
 #include <QList>
 #include <QString>
+#include <QUrl>
 
 #include "httpproxy.h"
 
 
 QList<HttpProxy> loadProxiesFromFile(const QString& filePath);
 void saveProxiesToFile(const QString& filePath, const QList<HttpProxy>& proxies);
+
+QList<QUrl> loadUrlsFromFile(const QString& filePath);
+void saveUrlsToFile(const QString& filePath, const QList<QUrl>& urls);
+
+bool is_valid_url(const std::string& url);
